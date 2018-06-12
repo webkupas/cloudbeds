0ef213b9999cccde8b1d9fa66da8b1076a25a4d4
Pavel Bambollo <webkupas@gmail.com>
2018-06-12 20:24:42 +0300

    heroku fix

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
 
 const port = process.env.PORT || 8080
 let app = express()
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-  // res.send(path.join(__dirname, 'dist', 'index.html'))
+  res.send(path.join(__dirname, 'dist', 'index.html'))
 })
 app.listen(port, () => {
   console.log(`Server is start on port ${port}`)
